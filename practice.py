f5d3a4fc6cb975b440cecb716dc11082c3215192
SangYoonLee <sylagape1231@gmail.com>
2022-03-11 17:26:28 +0900

    Update All
    2022-03-11 PS-Note
    ⊙ 백준 1063번 : 킹
    풀이 기록 완료

--- practice.py
+++ practice.py
@@ -1,17 +1,63 @@
-a, b = tuple(map(int, input().split()))
+place = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
-cnt = 0; ans = 0
+dxs = [0, 1, 1, 1, 0, -1, -1, -1]
+dys = [1, 1, 0, -1, -1, -1, 0, 1]
 
-def count():
-    global cnt, ans
-    
-    for i in range(1, 1001):
-        for j in range(1, i+1):
-            cnt += 1
-            if a <= cnt and cnt <= b:
-                ans += i
-            if cnt > b:
-                return
+king, stone, n = tuple(input().split())
+n = int(n)
 
-count()
-print(ans)
+king_x = place.index(king[0]) + 1
+king_y = int(king[1])
+
+stone_x = place.index(stone[0]) + 1
+stone_y = int(stone[1])
+
+mapper = {
+    'T' : 0,
+    'RT' : 1,
+    'R' : 2,
+    'RB' : 3,
+    'B' : 4,
+    'LB' : 5,
+    'L' : 6,
+    'LT' : 7
+}
+
+def in_range(x, y):
+    return x >= 1 and x <= 8 and y >= 1 and y <= 8
+
+def move(dir_num):
+    global king_x, king_y, stone_x, stone_y
+
+    king_nx = king_x + dxs[dir_num]
+    king_ny = king_y + dys[dir_num]
+
+    if in_range(king_nx, king_ny):
+        king_x = king_nx
+        king_y = king_ny
+    else:
+        return
+
+    if king_x == stone_x and king_y == stone_y:
+        stone_nx = stone_x + dxs[dir_num]
+        stone_ny = stone_y + dys[dir_num]
+
+        if not in_range(stone_nx, stone_ny):
+            king_x = king_x - dxs[dir_num]
+            king_y = king_y - dys[dir_num]
+        else:
+            stone_x = stone_nx
+            stone_y = stone_ny
+
+
+for _ in range(n):
+    direction = input()
+    dir_num = mapper[direction]
+    move(dir_num)
+
+# 출력
+king_x = place[king_x - 1]
+print(king_x + str(king_y))
+
+stone_x = place[stone_x - 1]
+print(stone_x + str(stone_y))
