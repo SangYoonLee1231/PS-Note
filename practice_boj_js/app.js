842ce8e638b3802769885bb0e20782178fb60846
SangYoonLee <prestro1231@hufs.ac.kr>
2022-12-13 15:06:22 +0900

    ADD: 백준 25304번 추가

--- practice_boj_js/app.js
+++ practice_boj_js/app.js
@@ -1,25 +1,69 @@
-const fs = require("fs");
-// const input = fs.readFileSync("./input.txt", "utf8");
-const input = fs.readFileSync("dev/stdin", "utf8");
+// const fs = require("fs");
+// const input = fs.readFileSync("./input.txt", "utf8").toString();
+// // const input = fs.readFileSync("/dev/stdin", "utf8").toString();
 
-let inputTestCase = input.split(" ");
-inputTestCase = inputTestCase.map((item) => parseInt(item));
+// const inputTestCase = input.split("\n");
+// const n = Number(inputTestCase[0]);
+// const room = [];
+
+// for (let i = 1; i <= n; i++) {
+//   room.push(inputTestCase[i].split(""));
+// }
+
+// function solution(n, room) {
+//   let [availableSeatRow, availableSeatCol] = [0, 0];
+
+//   availableSeatRow = countAvailableSeat(n, room, "row");
+//   availableSeatCol = countAvailableSeat(n, room, "col");
+
+//   console.log(`${availableSeatRow} ${availableSeatCol}`);
+// }
+
+// function countAvailableSeat(n, room, dir) {
+//   let availableSeatCount = 0;
+
+//   for (let i = 0; i < n; i++) {
+//     let straight = 0;
+
+//     for (let j = 0; j < n; j++) {
+//       let [a, b] = dir === "row" ? [i, j] : [j, i];
+//       if (room[a][b] === ".") {
+//         straight += 1;
+//       } else {
+//         straight = 0;
+//       }
+//       if (straight >= 2) {
+//         availableSeatCount += 1;
+//         break;
+//       }
+//     }
+//   }
+
+//   return availableSeatCount;
+// }
+
+// solution(n, room);
+
+const fs = require("fs");
+// let input = fs.readFileSync("./input.txt", "utf8").toString().trim();
+let input = fs.readFileSync("/dev/stdin", "utf8").toString().trim();
+
+input = input.split("\n");
+let inputTestCase = input.map((item) => item.split(" "));
+// int로 전환
+inputTestCase = inputTestCase.map((item) => item.map((item) => Number(item)));
 
 function solution(inputTestCase) {
-  const [first, second, third] = inputTestCase;
-  let prize = 0;
+  const x = inputTestCase[0][0];
+  const n = inputTestCase[1][0];
+  let totalPrice = 0;
 
-  if (first === second && second === third) {
-    prize = 10000 + first * 1000;
-  } else if (first === second || first === third) {
-    prize = 1000 + first * 100;
-  } else if (second === third) {
-    prize = 1000 + second * 100;
-  } else {
-    prize = Math.max(...inputTestCase) * 100;
+  for (let i = 2; i < n + 2; i++) {
+    let [price, amount] = inputTestCase[i];
+    totalPrice += price * amount;
   }
 
-  console.log(prize);
+  console.log(totalPrice === x ? "Yes" : "No");
 }
 
 solution(inputTestCase);
