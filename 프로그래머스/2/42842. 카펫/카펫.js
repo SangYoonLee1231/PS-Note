992a4b3430f981444e0ffe87d27b49f03663e13e
SangYoonLee <prestro1231@hufs.ac.kr>
2023-11-29 16:16:48 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,13 +1,25 @@
+// function solution(brown, yellow) {
+//     let [row, col] = [0, 0];
+    
+//     for (let col = 1; col < 5000; col++) {
+//         for (let row = 1; row < 5000; row++) {
+//             if ((row * 2) + (col * 2) - 4 == brown && (row - 2) * (col - 2) == yellow) {
+//                 return [row, col]
+//             }     
+//         }  
+//     }
+// }
+
 function solution(brown, yellow) {
     let [row, col] = [0, 0];
     
-    for (let col = 1; col < 5000; col++) {
-        for (let row = 1; row < 5000; row++) {
-            if ((row * 2) + (col * 2) - 4 == brown && (row - 2) * (col - 2) == yellow) {
+    for (let col = 0; col < (brown+yellow); col++) {
+        const row = Math.floor((brown + yellow) / col);
+        
+        if ((row * 2) + (col * 2) - 4 == brown) {
+            if ((row - 2) * (col - 2) == yellow) {
                 return [row, col]
-            }     
-        }  
+            }
+        }
     }
-    
-    // return answer;
 }
