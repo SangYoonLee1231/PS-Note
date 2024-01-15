d2a5fa8a3722bc0a613fa2661f2c1eae4d544584
SangYoonLee <prestro1231@hufs.ac.kr>
2024-01-15 23:38:48 +0900

    [Bronze II] Title: 소수, Time: 48 ms, Memory: 33240 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,30 @@
+import math
+
+m = int(input())
+n = int(int(input()))
+prime_min = 10001
+prime_sum = 0
+
+
+def check_prime_num(num):
+    if num == 1:
+        return False
+    if num == 2:
+        return True
+
+    for i in range(2, int(math.sqrt(num)) + 1):
+        if num % i == 0:
+            return False
+    return True
+
+
+for i in range(m, n + 1):
+    if check_prime_num(i):
+        prime_min = min(prime_min, i)
+        prime_sum += i
+
+if prime_min == 10001:
+    print(-1)
+else:
+    print(prime_sum)
+    print(prime_min)
