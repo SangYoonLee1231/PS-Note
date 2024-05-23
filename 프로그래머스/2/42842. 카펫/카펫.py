2f15235928138196c167a92917aa83e613378093
SangYoonLee (SYL) <prestro1231@hufs.ac.kr>
2024-05-23 11:24:26 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -1,14 +1,10 @@
 import math
 
-def check(row, col, brown):
-    return (row + col - 2) * 2 == brown
-
-
 def solution(brown, yellow):
     # brown + yellow = row * col
     # brown = (row + col - 2) * 2
     
     for i in range(2, int(math.sqrt(brown + yellow)) + 1):
         if (brown + yellow) % i == 0:
-            if check((brown + yellow) // i, i, brown):
+            if (((brown + yellow) // i) + (i) - 2) * 2 == brown:
                 return [(brown + yellow) // i, i]
