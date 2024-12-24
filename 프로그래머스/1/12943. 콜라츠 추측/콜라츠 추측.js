91ccdc1f6b8181cb23fdd1c4ab77aba62d15a604
Sang Yoon Lee / SYL <prestro1231@hufs.ac.kr>
2024-12-24 16:28:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.21 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -1,21 +1,11 @@
 function solution(num) {
     count = 0;
     
-    while(num !== 1) {
-        if (num % 2) {
-            // 홀수
-            num = num * 3 + 1
-        } else {
-            // 짝수
-            num = parseInt(num / 2)
-        }
+    while(num !== 1 && count <= 500) {
+        (num % 2) ? (num = num * 3 + 1) : (num = parseInt(num / 2));
         
         count += 1;
-        
-        if (count > 500) {
-            return -1;
-        }
     }
     
-    return count;
+    return num === 1 ? count : -1;
 }
