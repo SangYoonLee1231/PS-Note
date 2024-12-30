2cc654b423487e8cd92750d26223894f5b1828a3
Sang Yoon Lee / SYL <prestro1231@hufs.ac.kr>
2024-12-31 04:05:22 +0900

    [level 1] Title: 폰켓몬, Time: 0.55 ms, Memory: 34.2 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,13 +1,6 @@
 function solution(nums) {
-    const map = new Map();
+    const nums_set_len = new Set(nums).size;
+    const half_len = nums.length / 2;
     
-    nums.forEach((elem) => {
-        map.set(elem, (map.get(elem) || 0) + 1);
-    })
-    
-    if (parseInt(nums.length / 2) <= map.size) {
-        return parseInt(nums.length / 2);
-    } else {
-        return map.size;
-    }
+    return half_len < nums_set_len ? half_len : nums_set_len;
 }
