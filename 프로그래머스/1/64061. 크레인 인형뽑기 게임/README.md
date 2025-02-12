f89ad70df7377c0858c48ece34d44a57e99105dc
Sang Yoon Lee / SYL <prestro1231@hufs.ac.kr>
2025-02-12 14:16:28 +0900

    [level 1] Title: 크레인 인형뽑기 게임, Time: 1.50 ms, Memory: 36.2 MB -BaekjoonHub

--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/README.md
@@ -1,10 +1,10 @@
 # [level 1] 크레인 인형뽑기 게임 - 64061 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/64061) 
 
 ### 성능 요약
 
-메모리: 36.3 MB, 시간: 1.32 ms
+메모리: 36.2 MB, 시간: 1.50 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 12일 12:32:27
+2025년 02월 12일 14:16:26
 
 ### 문제 설명
 
--- 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
+++ 프로그래머스/1/64061. 크레인 인형뽑기 게임/크레인 인형뽑기 게임.js
@@ -1,33 +1,32 @@
 function solution(board, moves) {
-    const size = board.length;
-    const basket = [];
-    
-    return moves.reduce((answer, move) => {
-        answer += move_to_basket(board, basket, move - 1, size);
-        return answer;
-    }, 0)
+  const basket = [];
+  let answer = 0;
+
+  for (const move of moves) {
+    answer += moveToBasket(board, basket, move - 1);
+  }
+
+  return answer;
 }
 
-function check_if_blow(basket) {
-    if (basket.length >= 2 && basket.at(-1) === basket.at(-2)) {
-        basket.pop();
-        basket.pop();
-        return 2;
+function checkIfBlow(basket) {
+  if (basket.length >= 2 && basket.at(-1) === basket.at(-2)) {
+    basket.pop();
+    basket.pop();
+    return 2;
+  }
+  return 0;
+}
+
+function moveToBasket(board, basket, move) {
+  for (let row = 0; row < board.length; row++) {
+    if (board[row][move]) {
+      basket.push(board[row][move]);
+      board[row][move] = 0;
+      return checkIfBlow(basket);
     }
+  }
     return 0;
 }
-
-function move_to_basket(board, basket, move, size) {
-    for (let i = 0; i < size; i++) {
-        if (board[i][move]) {
-            basket.push(board[i][move]);
-            board[i][move] = 0;
-            return check_if_blow(basket);
-        }
-    }
-    return 0;
-}
-
-
 
 // board[row][col] -> row: 1->size, col: move
